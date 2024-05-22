4b36e994592aefd60daaa21711a7367c4010e539
zhunushalieardak <zhunushalieardak@gmail.com>
2024-05-22 14:38:05 +0600

    delete the um

--- src/Pages/Delivery/Delivery.js
+++ src/Pages/Delivery/Delivery.js
@@ -4,7 +4,6 @@ function Delivery() {
   return (
     <section className="Delivery">
       <div className='container'>
-        <img className='Delivery_img'></img>
         <h1>We are committed to providing our customers with convenient and reliable shipping to make your purchase as pleasant as possible. We offer nationwide shipping with convenient options including standard shipping and express shipping for those who appreciate speed in receiving their order. Our team of professionals works hard to ensure that your order is processed promptly and delivered to you as soon as possible. We also offer a convenient order tracking system so that you can always be aware of the delivery status. We hope that our service will meet your expectations and you will come back to us again for quality shoes and excellent service</h1>
       </div>
     </section>
